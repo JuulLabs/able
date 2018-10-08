56f004194a426f2fcd83089d5d7c1c4b85b98f36
Travis Wyatt <travis.i.wyatt@gmail.com>
2018-10-08 10:53:56 -0700

    Remove @Throws from String.toUuid extension function (#13)
    The underlying `UUID.fromString` method does not declare throwing
    `IllegalArgumentException` (as a checked exception) so it is unnecessary
    to mark the extension function.

--- core/src/main/java/Uuid.kt
+++ core/src/main/java/Uuid.kt
@@ -9,5 +9,4 @@ import java.util.UUID
 /**
  * @throw [IllegalArgumentException] if receiver does not conform to the string representation as described in [UUID.toString].
  */
-@Throws(IllegalArgumentException::class)
 fun String.toUuid(): UUID = UUID.fromString(this)
