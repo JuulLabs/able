3fb43b4740f6118dac5328f8ee349ac5879bbae2
Travis Wyatt <travis.i.wyatt@gmail.com>
2018-10-22 16:32:14 -0700

    Add CoroutinesGattDevice and BluetoothDevice extension functions (#20)
    Provides a thread safe mechanism of having a `BluetoothDevice` directly
    associated with a `Gatt`, so that `BluetoothDevice` can be treated as a
    connectable element.
    
    Via extension functions, the connection state of a "BluetoothDevice" can
    be queried, and the state persists across connections (transcends `Gatt`
    objects).

---
+++ device/src/main/java/android/BluetoothDevice.kt
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2018 JUUL Labs, Inc.
+ */
+
+@file:Suppress("unused")
+
+package com.juul.able.experimental.device.android
+
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
+import android.bluetooth.BluetoothGattDescriptor
+import android.bluetooth.BluetoothGattService
+import android.content.Context
+import com.juul.able.experimental.ConnectGattResult
+import com.juul.able.experimental.GattStatus
+import com.juul.able.experimental.WriteType
+import com.juul.able.experimental.device.CoroutinesGattDevices
+import com.juul.able.experimental.messenger.GattCallbackConfig
+import com.juul.able.experimental.messenger.OnCharacteristicChanged
+import com.juul.able.experimental.messenger.OnCharacteristicRead
+import com.juul.able.experimental.messenger.OnConnectionStateChange
+import com.juul.able.experimental.messenger.OnDescriptorWrite
+import com.juul.able.experimental.messenger.OnMtuChanged
+import kotlinx.coroutines.experimental.channels.BroadcastChannel
+import java.util.UUID
+
+private val BluetoothDevice.coroutinesGattDevice
+    get() = CoroutinesGattDevices.wrapped(this)
+
+fun BluetoothDevice.isConnected(): Boolean = coroutinesGattDevice.isConnected()
+
+suspend fun BluetoothDevice.connect(
+    context: Context,
+    autoConnect: Boolean,
+    callbackConfig: GattCallbackConfig = GattCallbackConfig()
+): ConnectGattResult = coroutinesGattDevice.connect(context, autoConnect, callbackConfig)
+
+val BluetoothDevice.onConnectionStateChange: BroadcastChannel<OnConnectionStateChange>
+    get() = coroutinesGattDevice.onConnectionStateChange
+
+val BluetoothDevice.onCharacteristicChanged: BroadcastChannel<OnCharacteristicChanged>
+    get() = coroutinesGattDevice.onCharacteristicChanged
+
+val BluetoothDevice.services: List<BluetoothGattService>
+    get() = coroutinesGattDevice.services
+
+fun BluetoothDevice.requestConnect(): Boolean = coroutinesGattDevice.requestConnect()
+
+fun BluetoothDevice.requestDisconnect(): Unit = coroutinesGattDevice.requestDisconnect()
+
+fun BluetoothDevice.getService(uuid: UUID): BluetoothGattService? =
+    coroutinesGattDevice.getService(uuid)
+
+suspend fun BluetoothDevice.connect(): Boolean = coroutinesGattDevice.connect()
+
+suspend fun BluetoothDevice.disconnect(): Unit = coroutinesGattDevice.disconnect()
+
+suspend fun BluetoothDevice.discoverServices(): GattStatus = coroutinesGattDevice.discoverServices()
+
+suspend fun BluetoothDevice.readCharacteristic(
+    characteristic: BluetoothGattCharacteristic
+): OnCharacteristicRead = coroutinesGattDevice.readCharacteristic(characteristic)
+
+suspend fun BluetoothDevice.writeCharacteristic(
+    characteristic: BluetoothGattCharacteristic,
+    value: ByteArray,
+    writeType: WriteType = WRITE_TYPE_DEFAULT
+) = coroutinesGattDevice.writeCharacteristic(characteristic, value, writeType)
+
+suspend fun BluetoothDevice.writeDescriptor(
+    descriptor: BluetoothGattDescriptor,
+    value: ByteArray
+): OnDescriptorWrite = coroutinesGattDevice.writeDescriptor(descriptor, value)
+
+suspend fun BluetoothDevice.requestMtu(mtu: Int): OnMtuChanged =
+    coroutinesGattDevice.requestMtu(mtu)
+
+fun BluetoothDevice.setCharacteristicNotification(
+    characteristic: BluetoothGattCharacteristic,
+    enable: Boolean
+): Boolean = coroutinesGattDevice.setCharacteristicNotification(characteristic, enable)
+
+fun BluetoothDevice.close() {
+    coroutinesGattDevice.close()
+}
