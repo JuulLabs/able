317485cf5461e90eae965b82213f7c9858b31d30
Cedrick Cooke <development@cedrickc.net>
2019-08-23 09:43:46 -0700

    Improve runtime by building all reports with a single gradle invocation & improve CircleCI logging

--- scripts/jacoco-code-climate.sh
+++ scripts/jacoco-code-climate.sh
@@ -1,28 +1,32 @@
 #! /bin/bash
 
+# Define a helper so logs show what command is being ran
+function printExec { echo "\$ $@"; $@; }
+
 CC_CMD=$(pwd)/cc-test-reporter
 CC_DIR=$(pwd)/tmp-cc
 mkdir $CC_DIR
 $CC_CMD before-build
+# Generate jacoco reports for whole project
+printExec ./gradlew $GRADLE_ARGS jacocoTestReport
 # For every gradle subproject...
 for project in *; do
   if [ -f $project/build.gradle ]; then
     cd $project
-    # ...generate jacoco reports...
-    ../gradlew $GRADLE_ARGS jacocoTestReport
+    # ...for every report...
     for report in build/reports/jacoco/*; do
       if [ -d $report ]; then
         # ...format them for code-climate, and save to a temp dir...
         reportFile=$report/*.xml
         JACOCO_SOURCE_PATH=$(echo src/*/java)
         export JACOCO_SOURCE_PATH
-        $CC_CMD format-coverage -t jacoco -o $CC_DIR/codeclimate.$project.$(basename $reportFile .xml).json $reportFile
+        printExec $CC_CMD format-coverage -t jacoco -o $CC_DIR/codeclimate.$project.$(basename $reportFile .xml).json $reportFile
       fi
     done
     cd ../
   fi
 done
 # ...then aggregate and upload
-$CC_CMD sum-coverage $CC_DIR/codeclimate.*.json -o $CC_DIR/codeclimate.json
-$CC_CMD upload-coverage --id $CODE_CLIMATE_TEST_REPORTER_ID --input $CC_DIR/codeclimate.json
+printExec $CC_CMD sum-coverage $CC_DIR/codeclimate.*.json -o $CC_DIR/codeclimate.json
+printExec $CC_CMD upload-coverage --id $CODE_CLIMATE_TEST_REPORTER_ID --input $CC_DIR/codeclimate.json
 rm -r $CC_DIR
