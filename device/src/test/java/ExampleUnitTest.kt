03b83fcd5cec77eb717144f49922e1bc3e596ed4
Travis Wyatt <travis@juul.com>
2020-02-04 10:58:16 -0800

    Integrate JaCoCo code coverage reporting and upload to codecov (#42)

---
+++ device/src/test/java/ExampleUnitTest.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2020 JUUL Labs, Inc.
+ */
+
+package com.juul.able.experimental.device.test
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
