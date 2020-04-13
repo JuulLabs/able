7ce2b129600b954f815e12b1182f3a34b4b34414
Travis Wyatt <travis.i.wyatt@gmail.com>
2020-04-12 18:50:31 -0700

    Simplify tag generation in timber-logger
    Following suit of JakeWharton/timber#357, simplified the mechanism of
    determining the log tag (method name for the log message) by filtering
    out TimberLogger classes rather than relying on position of stack
    elements.

--- timber-logger/src/main/java/TimberLogger.kt
+++ timber-logger/src/main/java/TimberLogger.kt
@@ -9,24 +9,30 @@ import com.juul.able.logger.Logger
 import timber.log.Timber
 
 private const val MAX_TAG_LENGTH = 23
+private val ANONYMOUS_CLASS_REGEX = "(\\$\\d+)+$".toRegex()
 
 class TimberLogger : Logger {
-
-    private val tag = AndroidTagGenerator()
 
     override fun isLoggable(priority: Int) = Timber.treeCount() > 0
 
     override fun log(priority: Int, throwable: Throwable?, message: String) {
         Timber
-            .tag(tag.generate().asSafeTag())
+            .tag(generateTag())
             .log(priority, throwable, message)
     }
-}
 
-// Tag length limit was removed in API 24.
-private fun String.asSafeTag(): String =
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N && length > MAX_TAG_LENGTH) {
-        substring(0, MAX_TAG_LENGTH)
-    } else {
-        this
+    private fun generateTag(): String {
+        val stackTraceElement = Throwable().stackTrace
+            .first { it.className != TimberLogger::class.java.name }
+
+        val tag = ANONYMOUS_CLASS_REGEX
+            .replace(stackTraceElement.className.substringAfterLast('.'), "")
+
+        // Tag length limit was removed in API 24.
+        return if (tag.length <= MAX_TAG_LENGTH || Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            tag
+        } else {
+            tag.substring(0, MAX_TAG_LENGTH)
+        }
     }
+}
